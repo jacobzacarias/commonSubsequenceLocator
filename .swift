7a3bc11ea1b5ee7acacfc6b6b9982742ef6df9b8
Jacob Zacarias <101657772+jacobzacarias@users.noreply.github.com>
2023-06-24 06:14:06 -0600

    Create .swift
    This commit is in Swift for the purpose of making a folder structure for this common sequence locator. The Models folder contains the necessary models for the program to define the data structures and algorithms necessary for finding the common sequence. The Algorithms folder includes the classes LongestCommonSequence and SubsequenceLocator that use algorithms for finding the appropriate sequence. The Tests folder contains unit tests for models and algorithms to guarantee their reliability. The main.swift file serves as the entry point of the program, written in a coding language that's tailored for iOS mobile devices.

---
+++ .swift
@@ -0,0 +1,48 @@
+// CommonSequenceLocator Folder Structure
+
+// Models folder
+class Sequence {
+    // Implementation of the Sequence model
+}
+
+class Locator {
+    // Implementation of the Locator model
+}
+
+// Helpers folder
+class InputParser {
+    // Implementation of the InputParser helper class
+}
+
+class OutputFormatter {
+    // Implementation of the OutputFormatter helper class
+}
+
+// Algorithms folder
+class LongestCommonSequence {
+    // Implementation of the LongestCommonSequence algorithm
+}
+
+class SubsequenceLocator {
+    // Implementation of the SubsequenceLocator algorithm
+}
+
+// Tests folder
+class SequenceTests {
+    // Implementation of the Sequence unit tests
+}
+
+class LocatorTests {
+    // Implementation of the Locator unit tests
+}
+
+class AlgorithmTests {
+    // Implementation of the Algorithm unit tests
+}
+
+// main.swift
+func main() {
+    // Entry point of the program
+}
+
+main()
